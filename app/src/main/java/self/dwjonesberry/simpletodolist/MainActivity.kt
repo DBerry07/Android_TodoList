3727ecf6bfeda1513aca69ce6e73a7db2dda7b97
David W. Berry <david.wj.berry@gmail.com>
2024-06-24 16:47:05 -0400

    implemented my app

--- app/src/main/java/self/dwjonesberry/simpletodolist/MainActivity.kt
+++ app/src/main/java/self/dwjonesberry/simpletodolist/MainActivity.kt
@@ -17,31 +17,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             SimpleToDoListTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MyApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SimpleToDoListTheme {
-        Greeting("Android")
     }
 }

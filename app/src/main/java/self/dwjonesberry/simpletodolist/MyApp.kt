2add237cbdae40f3ab609c6b56665283d5ad8599
David W. Berry <david.wj.berry@gmail.com>
2024-06-24 16:46:47 -0400

    Create MyApp.kt

---
+++ app/src/main/java/self/dwjonesberry/simpletodolist/MyApp.kt
@@ -0,0 +1,16 @@
+package self.dwjonesberry.simpletodolist
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun MyApp() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Screens.MAIN.name) {
+        composable(Screens.MAIN.name) { MainScreen() { navController.navigate(Screens.ADD.name)} }
+        composable(Screens.ADD.name) { AddTodoScreen() }
+    }
+}

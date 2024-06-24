84842a1338e5c0b418ddd1b14381236066b95949
David W. Berry <david.wj.berry@gmail.com>
2024-06-24 16:03:54 -0400

    basic ui for add todo item to list

--- app/src/main/java/self/dwjonesberry/simpletodolist/AddToDoScreen.kt
+++ app/src/main/java/self/dwjonesberry/simpletodolist/AddToDoScreen.kt
@@ -1,14 +1,56 @@
 package self.dwjonesberry.simpletodolist
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun AddTodoScreen() {
+    Column {
+        AddActionBar()
+        AddTodoText()
+    }
+}
 
+@Composable
+fun AddActionBar() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text("Hello")
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text("Goodbye")
+        }
+    }
+}
+
+@Composable
+fun AddTodoText() {
+    var text by remember { mutableStateOf("Hello") }
+
+    TextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = text,
+        onValueChange = { text = it }
+    )
 }
 
 @Preview

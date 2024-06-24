fb73e15941763bf2f04e60df3d5abd412c07e6be
David W. Berry <david.wj.berry@gmail.com>
2024-06-24 15:57:53 -0400

    Create AddToDoScreen.kt

---
+++ app/src/main/java/self/dwjonesberry/simpletodolist/AddToDoScreen.kt
@@ -0,0 +1,20 @@
+package self.dwjonesberry.simpletodolist
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AddTodoScreen() {
+
+}
+
+@Preview
+@Composable
+fun ATSPreview() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        AddTodoScreen()
+    }
+}

f9efc4c4e9d9b75007bf286120072de76c77d8f9
David W. Berry <david.wj.berry@gmail.com>
2024-06-25 13:34:56 -0400

    wrapped navhost in scaffold

--- app/src/main/java/self/dwjonesberry/simpletodolist/MyApp.kt
+++ app/src/main/java/self/dwjonesberry/simpletodolist/MyApp.kt
@@ -1,6 +1,9 @@
 package self.dwjonesberry.simpletodolist
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -9,8 +12,14 @@ import androidx.navigation.compose.rememberNavController
 fun MyApp() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = Screens.MAIN.name) {
-        composable(Screens.MAIN.name) { MainScreen() { navController.navigate(Screens.ADD.name)} }
-        composable(Screens.ADD.name) { AddTodoScreen() { navController.popBackStack() } }
+    Scaffold { padding ->
+         NavHost(
+             modifier = Modifier.padding(padding),
+             navController = navController,
+             startDestination = Screens.MAIN.name
+         ) {
+            composable(Screens.MAIN.name) { MainScreen() { navController.navigate(Screens.ADD.name)} }
+            composable(Screens.ADD.name) { AddTodoScreen() { navController.popBackStack() } }
+        }
     }
 }

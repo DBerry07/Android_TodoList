7ce57614db1ddaacca291c57e5095be7fd251d3e
David W. Berry <david.wj.berry@gmail.com>
2024-07-01 10:37:53 -0400

    themed list items and changed to cards

--- app/src/main/java/self/dwjonesberry/simpletodolist/screens/MainScreen.kt
+++ app/src/main/java/self/dwjonesberry/simpletodolist/screens/MainScreen.kt
@@ -15,6 +15,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -124,13 +129,16 @@ fun ListItem(item: TodoItem, index: Int, deleteFromList: (Int) -> Unit) {
     var background by remember { mutableStateOf(Color.White) }
     var expanded by remember { mutableStateOf(false) }
     background = changeBackground(item.checked)
-    Box(
+    Card(
+        shape = MaterialTheme.shapes.medium,
+        colors = CardDefaults.cardColors().copy(
+            containerColor = background
+        ),
         modifier =
         Modifier
             .padding(horizontal = 10.dp, vertical = 5.dp)
-            .border(width = 1.dp, color = Color.Black)
+            .border(1.dp, Color.Black, shape = MaterialTheme.shapes.medium)
             .fillMaxWidth()
-            .background(background)
             .combinedClickable(onDoubleClick = {
                 expanded = !expanded
                 Log.d("MyProject", "item $index double clicked")

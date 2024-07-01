4a83a321e4f968d410aaa7a46aa175fc0632cc9f
David W. Berry <david.wj.berry@gmail.com>
2024-07-01 11:21:00 -0400

    styling

--- app/src/main/java/self/dwjonesberry/simpletodolist/screens/AddToDoScreen.kt
+++ app/src/main/java/self/dwjonesberry/simpletodolist/screens/AddToDoScreen.kt
@@ -1,14 +1,18 @@
 package self.dwjonesberry.simpletodolist.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,7 +20,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import self.dwjonesberry.simpletodolist.TodoViewModel
 
@@ -75,8 +81,15 @@ fun AddTodoText(holdingText: String, setText: (String) -> Unit) {
         text = holdingText
     }
 
-    TextField(
-        modifier = Modifier.fillMaxWidth(),
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 5.dp, vertical = 5.dp),
+        label = {Text("Title")},
+        colors = TextFieldDefaults.colors().copy(
+            unfocusedContainerColor = Color.Transparent,
+            focusedContainerColor = Color.Transparent,
+        ),
+        minLines = 2,
+        maxLines = 2,
         value = text,
         onValueChange = {
             setText.invoke(it)
@@ -91,7 +104,15 @@ fun AddTodoNotes(holdingNotes: String, setNotes: (String) -> Unit) {
     if (holdingNotes.isNotBlank()) {
         notes = holdingNotes
     }
-    TextField(value = notes, onValueChange = {
+    OutlinedTextField(value = notes, modifier = Modifier.fillMaxWidth().padding(horizontal = 5.dp, vertical = 5.dp),
+        label = {Text("Notes")},
+        minLines = 8,
+        maxLines = 8,
+        colors = TextFieldDefaults.colors().copy(
+            unfocusedContainerColor = Color.Transparent,
+            focusedContainerColor = Color.Transparent,
+        ),
+        onValueChange = {
         setNotes.invoke(it)
         notes = it
     })

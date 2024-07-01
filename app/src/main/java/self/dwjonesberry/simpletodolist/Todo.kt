541da65f3e1316c15e7fb7b258d416d341259595
David W. Berry <david.wj.berry@gmail.com>
2024-07-01 12:07:24 -0400

    added increase/decrease priority functions

--- app/src/main/java/self/dwjonesberry/simpletodolist/Todo.kt
+++ app/src/main/java/self/dwjonesberry/simpletodolist/Todo.kt
@@ -11,4 +11,17 @@ class Todo(
     @ColumnInfo(name = "notes") var notes: String = "",
     @ColumnInfo(name = "checked") var checked: Boolean = false,
     @ColumnInfo(name = "priority") var priority: Priority = Priority.NORMAL
-)
+) {
+    fun increasePriority() {
+        val current = this.priority.ordinal
+        if (current < Priority.entries.size - 1) {
+            this.priority = Priority.entries[current + 1]
+        }
+    }
+    fun decreasePriority() {
+        val current = this.priority.ordinal
+        if (current > 0) {
+            this.priority = Priority.entries[current - 1]
+        }
+    }
+}

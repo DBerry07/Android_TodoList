0aa4db17fb96832a7d5e00f8386071bfdc8887b6
David W. Berry <david.wj.berry@gmail.com>
2024-07-01 12:07:06 -0400

    added update function

--- app/src/main/java/self/dwjonesberry/simpletodolist/TodoDao.kt
+++ app/src/main/java/self/dwjonesberry/simpletodolist/TodoDao.kt
@@ -4,17 +4,21 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 
 @Dao
 interface TodoDao {
     @Query("SELECT * FROM todo")
     fun getAll(): List<Todo>
-    
+
     @Query("SELECT 1 FROM todo where id=(:pid)")
     fun getItem(pid: Int): Todo
 
     @Insert
     fun insert(vararg todos: Todo)
+
+    @Update
+    fun update(vararg todo: Todo)
 
     @Delete
     fun delete(vararg todos: Todo)

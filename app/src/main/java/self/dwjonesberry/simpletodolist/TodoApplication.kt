57269e9f1380a6049292147c0bc91bca2632992a
David W. Berry <david.wj.berry@gmail.com>
2024-07-01 17:54:15 -0400

    added repo

--- app/src/main/java/self/dwjonesberry/simpletodolist/TodoApplication.kt
+++ app/src/main/java/self/dwjonesberry/simpletodolist/TodoApplication.kt
@@ -2,9 +2,10 @@ package self.dwjonesberry.simpletodolist
 
 import android.app.Application
 import self.dwjonesberry.simpletodolist.room.TodoDatabase
+import self.dwjonesberry.simpletodolist.room.TodoRepository
 
 class TodoApplication: Application() {
 
     private val database by lazy { TodoDatabase.getInstance(this) }
-    // val repository by lazy { }
+    val repository by lazy { TodoRepository(database.todoDao()) }
 }

9d1e7a0f741e670d60b263136a7be57ea2c89691
David W. Berry <david.wj.berry@gmail.com>
2024-07-01 17:36:27 -0400

    fix build error

--- app/src/main/java/self/dwjonesberry/simpletodolist/TodoDao.kt
+++ app/src/main/java/self/dwjonesberry/simpletodolist/TodoDao.kt
@@ -11,7 +11,7 @@ interface TodoDao {
     @Query("SELECT * FROM todo")
     fun getAll(): List<Todo>
 
-    @Query("SELECT 1 FROM todo where id=(:pid)")
+    @Query("SELECT * FROM todo where id=(:pid)")
     fun getItem(pid: Int): Todo
 
     @Insert

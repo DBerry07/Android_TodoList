a07ea3d0312e97111b8c71de7e9cc546d829879f
David W. Berry <david.wj.berry@gmail.com>
2024-07-05 15:50:58 -0400

    added interchangeable app bar

--- app/src/main/java/self/dwjonesberry/simpletodolist/MyApp.kt
+++ app/src/main/java/self/dwjonesberry/simpletodolist/MyApp.kt
@@ -3,8 +3,11 @@ package self.dwjonesberry.simpletodolist
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -18,7 +21,9 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import self.dwjonesberry.simpletodolist.screens.AddToDoScreen
+import self.dwjonesberry.simpletodolist.screens.AppBarButton
 import self.dwjonesberry.simpletodolist.screens.MainScreen
+import self.dwjonesberry.simpletodolist.screens.TodoAppBar
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -29,28 +34,39 @@ fun MyApp() {
 
     @Composable
     fun ChangeAppBar(screen: Screens) {
+        val menuButton: @Composable () -> Unit = { AppBarButton(function = { /*TODO*/ }, icon = Icons.Default.Menu, description = "Menu button") }
+
         if (screen.name == Screens.MAIN.name) {
-            MyAppBar(
-                navigateToAddScreen = { navController.navigate(Screens.ADD.name) },
-                navigateToMainScreen = { navController.navigate(Screens.MAIN.name)} )
-                .Composable
+            TodoAppBar(
+                title = "Task List",
+                buttons = listOf(
+                    menuButton,
+                    { AppBarButton(function = { navController.navigate(Screens.ADD.name) }, icon = Icons.Default.Add, description = "Add todo button") },
+                )
+            )
+        } else if (screen.name == Screens.ADD.name) {
+            TodoAppBar(title = "Add a Task",
+                buttons = listOf(
+                    menuButton,
+                )
+            )
         }
     }
 
     Scaffold(topBar = { ChangeAppBar(screen = currentScreen) }) { padding ->
-            NavHost(
-                modifier = Modifier.padding(padding),
-                navController = navController,
-                startDestination = Screens.MAIN.name
-            ) {
-                composable(Screens.MAIN.name) {
-                    currentScreen = Screens.MAIN
-                    MainScreen().Screen
-                }
-                composable(Screens.ADD.name) {
-                    currentScreen = Screens.ADD
-                    AddToDoScreen({ navController.popBackStack() }).Screen
-                }
+        NavHost(
+            modifier = Modifier.padding(padding),
+            navController = navController,
+            startDestination = Screens.MAIN.name
+        ) {
+            composable(Screens.MAIN.name) {
+                currentScreen = Screens.MAIN
+                MainScreen().Screen
+            }
+            composable(Screens.ADD.name) {
+                currentScreen = Screens.ADD
+                AddToDoScreen({ navController.popBackStack() }).Screen
             }
         }
     }
+}
--- app/src/main/java/self/dwjonesberry/simpletodolist/screens/Components.kt
+++ app/src/main/java/self/dwjonesberry/simpletodolist/screens/Components.kt
@@ -1,14 +1,23 @@
 package self.dwjonesberry.simpletodolist.screens
 
+import android.util.Log
+import android.widget.Button
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import self.dwjonesberry.simpletodolist.MyAppBar
+import androidx.compose.ui.graphics.vector.ImageVector
 
 
 @Composable
@@ -26,7 +35,24 @@ fun ActionBar(map: HashMap<String, List<() -> Unit>>) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyAppBar() {
+fun TodoAppBar(title: String, buttons: List< @Composable () -> Unit>) {
+    val TAG = "TodoAppBar"
+    TopAppBar(
+        title = { Text(title) },
+        navigationIcon = { buttons[0].invoke() },
+        actions = {
+            for (index in 1..<buttons.size) {
+                buttons[index].invoke()
+            }
+        }
+    )
+}
 
+@Composable
+fun AppBarButton(function: () -> Unit, icon: ImageVector, description: String) {
+    IconButton(onClick = { function.invoke() }) {
+        Icon(icon, description)
+    }
 }

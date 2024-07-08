490f5b05ec2aef8561d0ff9df223a2d4cf54987a
David W. Berry <david.wj.berry@gmail.com>
2024-07-08 15:07:30 -0400

    added padding to action bar

--- app/src/main/java/self/dwjonesberry/simpletodolist/ui/composables/Components.kt
+++ app/src/main/java/self/dwjonesberry/simpletodolist/ui/composables/Components.kt
@@ -3,6 +3,7 @@ package self.dwjonesberry.simpletodolist.ui.composables
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -14,12 +15,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 import self.dwjonesberry.simpletodolist.ui.theme.PurpleGrey40
 
 
 @Composable
 fun ActionBar(buttons: List<Pair<@Composable () -> Unit, List<() -> Unit>>>) {
-    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
+    Row(modifier = Modifier.fillMaxWidth().padding(vertical = 5.dp), horizontalArrangement = Arrangement.SpaceAround) {
         for (pair in buttons) {
             Button(onClick = {
                 for (function in pair.second) {

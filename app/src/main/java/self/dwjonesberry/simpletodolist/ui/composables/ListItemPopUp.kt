35b06282d75b365bbfd9dcdbe0f3a6516fd19d45
David W. Berry <david.wj.berry@gmail.com>
2024-07-08 14:36:58 -0400

    change card colour based on checked or not

--- app/src/main/java/self/dwjonesberry/simpletodolist/ui/composables/ListItemPopUp.kt
+++ app/src/main/java/self/dwjonesberry/simpletodolist/ui/composables/ListItemPopUp.kt
@@ -1,6 +1,7 @@
 package self.dwjonesberry.simpletodolist.ui.composables
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,12 +18,15 @@ import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,8 +46,17 @@ fun ListItemPopUp(
     update: (TodoItem) -> Unit,
     delete: (TodoItem) -> Unit
 ) {
+    var background = Color.White
+    if (todoItem.checked) {
+        background = Color.LightGray
+    }
+
     Dialog(onDismissRequest = { onDismissRequest.invoke() }) {
-        Card(modifier = Modifier) {
+        Card(
+            modifier = Modifier, colors = CardDefaults.cardColors().copy(
+                containerColor = background
+            )
+        ) {
             Column(modifier = Modifier.padding(20.dp)) {
                 PopUpTextDisplay(modifier = Modifier, todoItem = todoItem)
                 PopUpActonBar(

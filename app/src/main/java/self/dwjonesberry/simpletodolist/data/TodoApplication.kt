50f5e5f8ce033fec42b8d2a8e700f1656bb665e0
David W. Berry <david.wj.berry@gmail.com>
2024-07-13 20:12:16 -0400

    removed unused files

--- app/src/main/java/self/dwjonesberry/simpletodolist/data/TodoApplication.kt
+++ app/src/main/java/self/dwjonesberry/simpletodolist/data/TodoApplication.kt
@@ -1,11 +1,6 @@
 package self.dwjonesberry.simpletodolist.data
 
 import android.app.Application
-import self.dwjonesberry.simpletodolist.room.TodoDatabase
-import self.dwjonesberry.simpletodolist.room.TodoRepository
 
 class TodoApplication: Application() {
-
-    private val database by lazy { TodoDatabase.getInstance(this) }
-    val repository by lazy { TodoRepository(database.todoDao()) }
 }

9236c20fd5dba031a9ad3ce22010d4dc8edd1435
David W. Berry <david.wj.berry@gmail.com>
2024-07-14 12:08:22 -0400

    added coloured tabs to each list item

--- app/src/main/java/self/dwjonesberry/simpletodolist/ui/composables/ListItem.kt
+++ app/src/main/java/self/dwjonesberry/simpletodolist/ui/composables/ListItem.kt
@@ -2,11 +2,17 @@ package self.dwjonesberry.simpletodolist.ui.composables
 
 import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CardDefaults
@@ -18,13 +24,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import self.dwjonesberry.simpletodolist.data.DummyTodo
+import self.dwjonesberry.simpletodolist.data.FirebaseRepository
 import self.dwjonesberry.simpletodolist.data.MyTask
 import self.dwjonesberry.simpletodolist.data.TaskViewModel
 
@@ -59,6 +69,7 @@ fun ListItem(
         ),
         modifier =
         Modifier
+            .height(55.dp)
             .padding(horizontal = 10.dp, vertical = 5.dp)
             .border(2.dp, border, shape = MaterialTheme.shapes.medium)
             .fillMaxWidth()
@@ -73,31 +84,33 @@ fun ListItem(
                 Log.d("MyProject", "item $index double clicked")
             }
     ) {
-        Column() {
-            Row(
-                modifier =
-                Modifier
-                    .padding(horizontal = 10.dp, vertical = 10.dp)
-            )
-            {
-                var str: String = item.id.toString()
-                if ((item.id) < 10) {
-                    str = "0$str"
-                }
-                if ((item.id) < 100) {
-                    str = "0$str"
-                }
-                str = "$str:"
-
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            var str: String = item.id.toString()
+            if ((item.id) < 10) {
+                str = "0$str"
+            }
+            if ((item.id) < 100) {
+                str = "0$str"
+            }
+            str = "$str:"
+            Column(modifier = Modifier.background(border).fillMaxHeight().padding(5.dp, 0.dp, 0.dp, 0.dp), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
                 Text(
-                    modifier = Modifier.width(60.dp),
+                    modifier = Modifier.width(70.dp).padding(horizontal = 10.dp),
                     text = str,
-                    color = border,
+                    color = Color.White,
                     fontSize = 20.sp,
                     fontWeight = FontWeight.Bold,
                     style = MaterialTheme.typography.displaySmall,
                     fontFamily = FontFamily.Monospace,
                 )
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier =
+                Modifier
+                    .padding(10.dp, 0.dp, 10.dp, 0.dp)
+            )
+            {
                 Text(
                     text = item.title,
                     fontSize = 20.sp,
@@ -117,4 +130,10 @@ fun ListItem(
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun ListItemPreview() {
+    ListItem(viewModel = TaskViewModel(FirebaseRepository()), index = 0, item = DummyTodo)
 }
